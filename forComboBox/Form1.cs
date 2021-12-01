4981067487e858e802b4fad1b952195766330593
Murat Karasah <mkarasah@gmail.com>
2021-12-01 14:41:24 +0300

    Add project files.

---
+++ forComboBox/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 7; i++)
+            {
+                ComboBox cmb = new ComboBox();
+                cmb.Location = new Point(5, i * 35);
+                cmb.BackColor = Color.FromArgb(140, 20*i,13*i);
+                cmb.Items.Add(i*40);
+                Controls.Add(cmb);
+            }
+            
+
+            
+        }
+    }
+}
